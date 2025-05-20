c0cc5f03ff48635a7632e3008b907fbdce81ed78
MrPerdej <antimorjat@gmail.com>
2025-05-20 17:52:35 +0300

    Update ugdi.c

--- ugdi.c
+++ ugdi.c
@@ -30,7 +30,7 @@ int main() {
 			break;
 	}
 	printf("Случайное число равно: %d\n", r); // 秘籍
-	printd("CHEAT MADE IN CHINA. SOCIAL CHINA\n")
+	printd("CHEAT MADE IN CHINA. 精實華人黨\n")
 	do {
 		printf("введите свои догадки:");
 		int dog;
